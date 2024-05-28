e1f5dc365324d4b10b78412ea035ca17b0a2b05d
seo02wow <rlatjdud9188@naver.com>
2024-05-28 13:55:00 +0900

    :sparkles:feat: API 연동으로 데이터 가져오기

--- codeitmall/pages/search.js
+++ codeitmall/pages/search.js
@@ -1,15 +1,30 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import { useRouter } from "next/router";
+import axios from "@/lib/axios";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   const router = useRouter();
   const { q } = router.query;
+  const [products, setProducts] = useState([]);
+
+  async function getProducts(query) {
+    const res = await axios.get(`/products/?q=${query}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
 
   return (
     <div>
       <h1>Search 페이지</h1>
       <SearchForm initialValue={q} />
       <h2>{q}검색 결과</h2>
+      <ProductList products={products} />
     </div>
   );
 }
